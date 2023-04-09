5a2d1ece00ebd2073f1c7c89e7fd61dc666502d2
Christos Karapasias <christos@marteco.nl>
2023-04-09 18:28:57 +0900

    Convenience wrapper

---
+++ Wavee.Audio.Vorbis/Convenience/SampleBuffer.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Wavee.Audio.Audio;
+using Wavee.Audio.Codecs;
+
+namespace Wavee.Audio.Vorbis.Convenience;
+
+/// <summary>
+/// A `SampleBuffer`, is a sample oriented buffer. It is agnostic to the ordering/layout of samples
+/// within the buffer. `SampleBuffer` is mean't for safely importing and exporting sample data to
+/// and from Symphonia using the sample's in-memory data-type.
+/// </summary>
+public sealed class SampleBuffer<S> where S : unmanaged
+{
+    private Memory<S> _buf;
+    private int _nWritten;
+
+    public SampleBuffer(ulong duration, SignalSpec spec)
+    {
+        // The number of channels * duration cannot exceed u64::MAX.
+        if (duration > ulong.MaxValue / spec.Channels.Count())
+            throw new ArgumentOutOfRangeException(nameof(duration));
+
+        // The total number of samples the buffer will store.
+        var nSamples = duration * spec.Channels.Count();
+
+
+        // Practically speaking, it is not possible to allocate more than usize::MAX bytes of
+        // samples. This assertion ensures the potential downcast of n_samples to usize below is
+        // safe.
+        if (nSamples > (ulong)(int.MaxValue / Marshal.SizeOf<S>()))
+            throw new ArgumentOutOfRangeException(nameof(duration));
+
+        // Allocate enough memory for all the samples and fill the buffer with silence.
+        _buf = new S[(int)nSamples];
+        _buf.Span.Fill(default);
+
+        _zeroes = new S[(int)nSamples];
+        _zeroes.Span.Fill(default);
+        _nWritten = 0;
+    }
+
+    public void CopyInterleavedRef(AudioBuffer<float> decoded)
+    {
+        var nChannels = decoded.Spec().Channels.Count();
+        var nSamples = (int)(decoded.Frames * nChannels);
+
+        // Ensure that the capacity of the sample buffer is greater than or equal to the number
+        // of samples that will be copied from the source buffer.
+        if (this.Capacity() < nSamples)
+            throw new ArgumentOutOfRangeException(nameof(decoded));
+
+        // Interleave the source buffer channels into the sample buffer.
+        for (int ch = 0; ch < nChannels; ch++)
+        {
+            var chSlice = decoded.Chan(ch);
+
+            for (int i = 0; i < chSlice.Length; i++)
+            {
+                var n = (int)(ch + i * nChannels);
+                var val = chSlice[i];
+                _buf.Span[n] = Unsafe.As<float, S>(ref val);
+            }
+        }
+
+        _nWritten = nSamples;
+    }
+
+    private long Capacity() => _buf.Length;
+
+    public ReadOnlySpan<S> Samples() =>
+        _nWritten != 0 ? _buf.Span[.._nWritten] : _zeroes.Span;
+
+    private static Memory<S> _zeroes;
+}
---
+++ Wavee.Audio.Vorbis/Convenience/VorbisReader.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices;
+using Wavee.Audio.Codecs;
+using Wavee.Audio.IO;
+
+namespace Wavee.Audio.Vorbis.Convenience;
+
+public sealed class VorbisReader : IDisposable
+{
+    private readonly bool _disposeOnClose;
+    private readonly Stream _reader;
+    private readonly MediaSourceStream _mediaSourceStream;
+
+    private readonly OggReader _oggReader;
+    private readonly VorbisDecoder _decoder;
+
+    private SampleBuffer<float>? _sampleBuffer;
+
+    public VorbisReader(Stream reader, bool disposeOnClose)
+    {
+        _reader = reader;
+        _disposeOnClose = disposeOnClose;
+        _mediaSourceStream = new MediaSourceStream(reader,
+            new MediaSourceStreamOptions(64 * 1024));
+        _oggReader = new OggReader(_mediaSourceStream, new FormatOptions());
+        _decoder = new VorbisDecoder(_oggReader.DefaultTrack!.CodecParameters,
+            new DecoderOptions(false));
+    }
+
+    public int SampleRate => (int)_oggReader.DefaultTrack!.CodecParameters.SampleRate!.Value;
+    public int Channels => (int)_oggReader.DefaultTrack!.CodecParameters.Channels!.Value.Count();
+
+    public ReadOnlySpan<byte> ReadSamples()
+    {
+        var packet = _oggReader.NextPacket();
+        var decoded = _decoder.Decode(packet);
+        if (_sampleBuffer is null)
+        {
+            _sampleBuffer = new SampleBuffer<float>(
+                (ulong)decoded.Capacity(),
+                decoded.Spec());
+        }
+
+        _sampleBuffer.CopyInterleavedRef(decoded);
+        var samples = _sampleBuffer.Samples();
+        var bytes = MemoryMarshal.Cast<float, byte>(samples);
+        return bytes;
+    }
+
+
+    public void Dispose()
+    {
+        if (_disposeOnClose)
+        {
+            _reader.Dispose();
+        }
+    }
+}
